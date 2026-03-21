1d3e1522f9bd1e6c2450b3e933799c2e6c3e2f2e
Diego Santana <3c.santana@gmail.com>
2026-03-21 15:51:05 -0300

    feat: Implementa mascara para o cep

--- js/utils/cep-utils.js
+++ js/utils/cep-utils.js
@@ -20,3 +20,22 @@ export function clearZipCode(cep) {
 export function isValidZipCode(cep) {
     return cep.length === 8;
 }
+
+/**
+ * Aplica uma máscara de CEP para o valor informado.
+ * Se o valor tiver menos de 5 caracteres, retorna o valor sem hífen.
+ * Se o valor tiver mais de 5 caracteres, coloca um hífen entre o 5º e o 6º caractere e retorna o valor.
+ * @param {string|number} valor O valor a ter a máscara de CEP aplicada.
+ * @returns {string} O valor com a máscara de CEP aplicada.
+ */
+export function applyCEPMask(valor) {
+  const cleanZipCode = String(valor).replace(/\D/g, '').slice(0, 8);
+
+  // Verifica se o CEP possui 5 (para inserir o hífen)
+  if (cleanZipCode.length <= 5) {
+    return cleanZipCode; // Não tem 5 caractecores, retorna sem hífen
+  }
+
+  // Tem mais de 5 caracteres, coloca o hífen e retorna o valor
+  return `${cleanZipCode.slice(0, 5)}-${cleanZipCode.slice(5)}`;
+}
