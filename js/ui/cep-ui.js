e4e021f7b0acd5938c8d2a699cf7c7b524b4d5c6
Diego Santana <3c.santana@gmail.com>
2026-03-21 07:58:19 -0300

    feat: Implementa a limpeza da msg de erro

--- js/ui/cep-ui.js
+++ js/ui/cep-ui.js
@@ -2,11 +2,20 @@
 // Manipulação da interface e do DOM
 // =================================================================
 
+// Elemento de erro
+const errorElement = document.querySelector("#error-message");
+
 /**
  * Renderiza uma mensagem de erro na interface.
  * @param {string} message A mensagem de erro a ser renderizada.
  */
 export function renderErrorMessage(message) {
-  const errorElement = document.querySelector("#error-message");
   errorElement.innerHTML = message;
+}
+
+/**
+ * Limpa a mensagem de erro da interface.
+ */
+export function clearErrorMessage() {
+  errorElement.innerHTML = "";
 }
