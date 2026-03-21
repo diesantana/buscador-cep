056b9afa0009d465c49a5a2c0e8c4637f40d989b
Diego Santana <3c.santana@gmail.com>
2026-03-21 08:36:24 -0300

    feat: Implementa a validação de cep não encontrado

--- js/services/viacep.js
+++ js/services/viacep.js
@@ -9,7 +9,12 @@
  * @returns {Promise<object>} Os dados do CEP em formato JSON.
  */
 export async function fetchCep(cep) {
-    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const data = await response.json();
-    return data;
+  const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  const data = await response.json();
+
+  if (data.erro) {
+    throw new Error("CEP não encontrado.");
+  }
+  
+  return data;
 }
