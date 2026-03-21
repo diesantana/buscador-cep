687a7c857a09e3f6a17fdc8954c5ad4631104125
Diego Santana <3c.santana@gmail.com>
2026-03-21 07:28:41 -0300

    feat: Implementa a limpeza do cep recebido no form

--- js/utils/cep-utils.js
+++ js/utils/cep-utils.js
@@ -1,1 +1,13 @@
+// =================================================================
 // Funções auxiliares para limpeza e validação do CEP
+// =================================================================
+
+/**
+ * Remove todos os caracteres que não são números do CEP.
+ * @param {string} cep O CEP a ser limpo.
+ * @returns {string} O CEP limpo.
+ */
+export function clearZipCode(cep) {
+    return cep.replace(/\D/g, '');
+    // /\D/g remove tudo que não for número
+}
