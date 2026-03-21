10c12ee675244d55eeaae3ba80999e982463120a
Diego Santana <3c.santana@gmail.com>
2026-03-21 08:06:04 -0300

    feat: Implementa a lógica para buscar o cep na API

--- js/services/viacep.js
+++ js/services/viacep.js
@@ -1,1 +1,15 @@
+// =================================================================
 // Comunicação com a API
+// =================================================================
+
+/**
+ * Faz uma requisição para a API do ViaCEP
+ * para buscar os dados de um CEP.
+ * @param {string} cep O CEP a ser buscado.
+ * @returns {Promise<object>} Os dados do CEP em formato JSON.
+ */
+export async function fetchCep(cep) {
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const data = await response.json();
+    return data;
+}
