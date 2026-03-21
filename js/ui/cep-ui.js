ee283b9a20438ab2f4ca0dcb6bb8d38110665c0a
Diego Santana <3c.santana@gmail.com>
2026-03-21 07:39:55 -0300

    feat: Implementa a renderização da mensagem de erro

--- js/ui/cep-ui.js
+++ js/ui/cep-ui.js
@@ -1,1 +1,12 @@
+// =================================================================
 // Manipulação da interface e do DOM
+// =================================================================
+
+/**
+ * Renderiza uma mensagem de erro na interface.
+ * @param {string} message A mensagem de erro a ser renderizada.
+ */
+export function renderErrorMessage(message) {
+  const errorElement = document.querySelector("#error-message");
+  errorElement.innerHTML = message;
+}
