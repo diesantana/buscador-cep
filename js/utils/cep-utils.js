2330ceebe1fc4d595ef93aa61ada159610762e18
Diego Santana <3c.santana@gmail.com>
2026-03-21 07:40:08 -0300

    feat: Implementa a validaçaõ do cep

--- js/utils/cep-utils.js
+++ js/utils/cep-utils.js
@@ -11,3 +11,12 @@ export function clearZipCode(cep) {
     return cep.replace(/\D/g, '');
     // /\D/g remove tudo que não for número
 }
+
+/**
+ * Verifica se o CEP possui 8 caracteres.
+ * @param {string} cep O CEP a ser verificado.
+ * @returns {boolean} Verdadeiro se o CEP tiver 8 caracteres, falso caso contrário.
+ */
+export function isValidZipCode(cep) {
+    return cep.length === 8;
+}
