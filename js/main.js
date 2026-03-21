f4d440ae29563fe3ff871e5e57752a7707039373
Diego Santana <3c.santana@gmail.com>
2026-03-21 07:54:37 -0300

    feat: Implementa a lógica para validar o cep informado

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+// =================================================================
 // Controla o fluxo da aplicação
+// =================================================================
+import { clearZipCode, isValidZipCode } from "./utils/cep-utils.js";
+import { renderErrorMessage } from "./ui/cep-ui.js";
+/*
+main.js
+    submit do form
+    → pega input.value
+    → limpa
+    → valida
+    → busca na API
+    → atualiza UI
+*/
+
+// Elementos
+const form = document.querySelector("#cep-form");
+const inputCEP = document.querySelector("#cep");
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const enteredValue = inputCEP.value; // Valor digitado
+    const cleanZipCode = clearZipCode(enteredValue); // Limpa o CEP
+
+    // Verifica se o CEP informado é um CEP válido
+    const isValid = isValidZipCode(cleanZipCode);
+
+    if (!isValid) {
+        // renderiza mensagem de erro 
+        renderErrorMessage("O CEP informado é inválido!");
+        return;
+    }
+
+    
+});
+
